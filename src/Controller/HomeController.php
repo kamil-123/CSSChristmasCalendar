054a116197f12379f7723c16492b6a271dbc3846
kamil <kamil.drozd@usetreno.cz>
2022-11-28 00:29:44 +0100

    generate gift

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,11 @@ use App\Entity\Person;
 use App\Entity\PersonGift;
 use App\Entity\Tag;
 use App\Form\PersonFormType;
+use App\Repository\PersonGiftRepository;
+use App\Repository\PersonRepository;
 use App\Service\UploaderHelper;
 use DateTime;
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Response;
@@ -123,5 +126,67 @@ class HomeController extends AbstractController
         return $this->render('home/summary.html.twig', [
             'person' => $person,
         ]);
+    }
+
+    /**
+     * @Route("/generate-gift/{token}", name="generate-gift")
+     */
+    public function generateGift(
+        string $token,
+        PersonGiftRepository $personGiftRepository,
+        PersonRepository $personRepository,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        $checkToken = $this->getParameter('check.token');
+        if ($checkToken !== $token) {
+            return new Response(null , Response::HTTP_OK, []);
+        }
+
+        $date = new DateTimeImmutable('+5days');
+        $month = (int) $date->format('m');
+        $day = (int) $date->format('d');
+
+        if ($month === 12) {
+            $personGifts = $personGiftRepository->findAll();
+            $personGiftByDay = array_filter($personGifts, function (PersonGift $personGift) use ($day) {
+                return $personGift->getDay() === $day;
+            });
+            if ($personGiftByDay === []) {
+                $usedPersonIds = [];
+                $usedGiftIds = [];
+                foreach ($personGifts as $personGift) {
+                    $usedPersonIds [] = $personGift->getPerson()->getId();
+                    $usedGiftIds [] = $personGift->getGift()->getId();
+                }
+
+                $persons = $personRepository->findBy([
+                    'active' => true,
+                ]);
+
+                $unUsedPersons = array_filter($persons, function (Person $person) use ($usedPersonIds) {
+                    return !in_array($person->getId(), $usedPersonIds);
+                });
+
+                $unUsedGifts = array_filter($persons, function (Person $person) use($usedGiftIds) {
+                    return !in_array($person->getId(), $usedGiftIds);
+                });
+
+                if ($unUsedPersons === [] || $unUsedGifts === []) {
+                    return new Response(null , Response::HTTP_OK, []);
+                }
+
+                shuffle($unUsedPersons);
+                shuffle($unUsedGifts);
+                $person = reset($unUsedPersons);
+                $gift = reset($unUsedGifts);
+                $newPersonGift = new PersonGift($day, $person, $gift);
+
+                $entityManager->persist($newPersonGift);
+                $entityManager->flush();
+            }
+        }
+
+        return new Response(null , Response::HTTP_OK, []);
     }
 }
